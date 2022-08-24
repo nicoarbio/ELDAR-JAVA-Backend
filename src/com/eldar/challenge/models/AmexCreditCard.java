0eefed6ec882502d32b02ad2fed6abbd0c898dee
Nicolás Arbio <nicolasarbio@outlook.com>
2022-08-23 23:17:21 -0300

    Adding models (classes)

---
+++ src/com/eldar/challenge/models/AmexCreditCard.java
@@ -0,0 +1,40 @@
+package com.eldar.challenge.models;
+
+import java.time.LocalDate;
+
+public final class AmexCreditCard extends CreditCard {
+	
+	private String brand = "AMEX";
+
+	public AmexCreditCard(String number, String cardholder, LocalDate expirationDate) {
+		super(number, cardholder, expirationDate);
+	}
+
+	@Override
+	public String getBrand() {
+		return this.brand;
+	}
+
+	@Override
+	public String getCardInformation() {
+		return "AmexCreditCard [brand=" + getBrand() + ", " + super.getCardInformation() + "]";
+	}
+
+	@Override
+	public String isDifferent(CreditCard creditCard) {
+		if(this.compareTo(creditCard) != 0)
+			return MSG_CARD_DIFF;
+		
+		return MSG_CARD_EQUAL;
+	}
+	
+	/**
+	 * AMEX Processing Fee = month * 0.1  return getExpirationDate().getMonthValue() * 0.1;
+	 */
+	@Override
+	public double getProcessingFee(double transaction) {
+		return transaction * ((double) adjustProcessingFee(
+				((double) LocalDate.now().getMonthValue() * 0.1)
+				) /100 );
+	}
+}
---
+++ src/com/eldar/challenge/models/NaraCreditCard.java
@@ -0,0 +1,40 @@
+package com.eldar.challenge.models;
+
+import java.time.LocalDate;
+
+public final class NaraCreditCard extends CreditCard {
+	
+	private String brand = "NARA";
+
+	public NaraCreditCard(String number, String cardholder, LocalDate expirationDate) {
+		super(number, cardholder, expirationDate);
+	}
+
+	@Override
+	public String getBrand() {
+		return this.brand;
+	}
+
+	@Override
+	public String getCardInformation() {
+		return "NaraCreditCard [brand=" + getBrand() + ", " + super.getCardInformation() + "]";
+	}
+
+	@Override
+	public String isDifferent(CreditCard creditCard) {
+		if(this.compareTo(creditCard) != 0)
+			return MSG_CARD_DIFF;
+		
+		return MSG_CARD_EQUAL;
+	}
+	
+	/**
+	 * NARA Processing Fee = day-of-month * 0.5
+	 */
+	@Override
+	public double getProcessingFee(double transaction) {
+		return transaction * ((double) adjustProcessingFee(
+				((double) LocalDate.now().getDayOfMonth() * 0.5)
+				) /100 );
+	}
+}
